bcd76bed1b141ef97910d1fa744e7f94ce4c8987
Lucas Charoing <charoing.lucas@gmail.com>
2023-01-26 21:39:36 +0100

    Add color picker UI

--- frontend/js/drawing.js
+++ frontend/js/drawing.js
@@ -1,6 +1,9 @@
 const socket = io("http://localhost:3000");
 
 const colorInputs = document.querySelectorAll('.color-input');
+const colorLabels = document.querySelectorAll('.color-input__label');
+const colorPicker = document.querySelector('#color-picker');
+const colorPickerLabel = document.querySelector('.color-picker__label');
 const widthInputs = document.querySelectorAll('.width-input');
 const undoButton = document.querySelector('.undo-button');
 const redoButton = document.querySelector('.redo-button');
@@ -17,11 +20,16 @@ let strokeWidth = parseInt(widthInputs[0].value);
 let previousX;
 let previousY;
 
-colorInputs.forEach(colorInput => {
+colorInputs.forEach((colorInput, index) => {
     colorInput.addEventListener('click', () => {
-        strokeStyle = colorInput.value;
+        colorPickerLabel.classList.remove('text-white/80', 'text-black/30');
+        colorPickerLabel.classList.add('text-transparent');
+        strokeStyle = window.getComputedStyle(colorLabels[index]).backgroundColor;
     })
 })
+
+colorPicker.addEventListener('change', updateColor);
+colorPicker.addEventListener('click', updateColor);
 
 widthInputs.forEach(widthInput => {
     widthInput.addEventListener('click', () => {
@@ -38,7 +46,9 @@ undoButton.addEventListener('click', () => {
             context.fillRect(0, 0, canvas.getBoundingClientRect().width, canvas.getBoundingClientRect().height);
         } else {
             stroke.moves.forEach((move, index) => {
+                context.beginPath();
                 drawLine(stroke.strokeStyle, stroke.strokeWidth, move.xPosition, move.yPosition, index-1 in stroke.moves ? stroke.moves[index-1].xPosition : move.xPosition, index-1 in stroke.moves ? stroke.moves[index-1].yPosition : move.yPosition);
+                context.closePath();
             })
         }
     })
@@ -52,7 +62,9 @@ redoButton.addEventListener('click', () => {
             context.fillRect(0, 0, canvas.getBoundingClientRect().width, canvas.getBoundingClientRect().height);
         } else {
             historySave[i].moves.forEach((move, index) => {
+                context.beginPath();
                 drawLine(historySave[i].strokeStyle, historySave[i].strokeWidth, move.xPosition, move.yPosition, index - 1 in historySave[i].moves ? historySave[i].moves[index - 1].xPosition : move.xPosition, index - 1 in historySave[i].moves ? historySave[i].moves[index - 1].yPosition : move.yPosition);
+                context.closePath();
             })
         }
     }
@@ -81,7 +93,11 @@ canvas.addEventListener('mousemove', (event) => {
         let yPosition = event.clientY - canvas.offsetTop;
         previousX ??= xPosition;
         previousY ??= yPosition;
+        context.beginPath();
         drawLine(strokeStyle, strokeWidth, xPosition, yPosition, previousX, previousY);
+        context.closePath();
+        context.fillStyle = '#8ED6FF';
+        context.fill();
         lastStroke.moves.push({
             xPosition: xPosition,
             yPosition: yPosition
@@ -92,14 +108,12 @@ canvas.addEventListener('mousemove', (event) => {
 })
 
 function drawLine(strokeStyle, strokeWidth, xPosition, yPosition, previousX, previousY) {
-    context.beginPath();
     context.lineCap = "round";
     context.strokeStyle = strokeStyle;
     context.lineWidth = strokeWidth;
     context.moveTo(previousX, previousY);
     context.lineTo(xPosition, yPosition);
     context.stroke();
-    context.closePath();
 }
 
 function endStroke() {
@@ -110,4 +124,23 @@ function endStroke() {
     isDrawing = false;
     previousX = null;
     previousY = null;
+}
+
+function updateColor() {
+    colorPickerLabel.classList.remove('text-white/80', 'text-black/30');
+    strokeStyle = colorPickerLabel.style.backgroundColor = colorPicker.value;
+    let color = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(colorPicker.value);
+    let [r, g, b] = [parseInt(color[1], 16), parseInt(color[2], 16), parseInt(color[3], 16)];
+    let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    console.log(luma);
+    if (luma < 160) {
+        colorPickerLabel.classList.add('text-white/80');
+    } else {
+        colorPickerLabel.classList.add('text-black/30');
+    }
+    console.log(r, g, b);
+    colorPickerLabel.classList.remove('text-transparent');
+    colorInputs.forEach(colorInput => {
+        colorInput.checked = false;
+    })
 }
