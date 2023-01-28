f378cc883779befab2e824573b33ed907b669ca7
Ali ABBAS <ali.ibra.abbas@gmail.com>
2023-01-28 23:27:52 +0100

    Login And Register System

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,26 +1,26 @@
-import express from "express";
-const router = express.Router();
+// import express from "express";
+// const router = express.Router();
 
 
-router.get("/", (req,res) => {
-    res.json({
-        name: "Fanny",
-        lastName: "Gautier",
-        email: "fanny@mail.com"
-    })
-})
+// router.get("/", (req,res) => {
+//     res.json({
+//         name: "Fanny",
+//         lastName: "Gautier",
+//         email: "fanny@mail.com"
+//     })
+// })
 
-router.post("/", (req,res) => {
-    res.send('Post a new user')
-})
+// router.post("/", (req,res) => {
+//     res.send('Post a new user')
+// })
 
-router.put("/", (req,res) => {
-    res.send('Modify a user')
-})
+// router.put("/", (req,res) => {
+//     res.send('Modify a user')
+// })
 
-router.delete("/", (req,res) => {
-    res.send('Delete a user')
-})
+// router.delete("/", (req,res) => {
+//     res.send('Delete a user')
+// })
 
 
-export default router;
+// export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,125 @@
 import express from "express";
-import { Server } from "socket.io";
+import path from "path";
 import bodyParser from "body-parser";
-import cors from "cors";
-import * as http from "http";
-import userRoute from './routes/user.js'
-const app= express();
+import mongoose from "mongoose";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+mongoose.connect('mongodb://localhost:27017/login-app-db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+})
+
+const app = express()
 const server = http.createServer(app);
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.json({hello: "world!"});
+
+app.post('/api/login', async (req, res) => {
+	const { username, password } = req.body
+	const user = await User.findOne({ username }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
 })
 
-const io = new Server(server, {
-    cors: {
-        origin: '*'
-    }
+app.post('/api/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
+
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await User.create({
+			username,
+			password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
+
+	res.json({ status: 'ok' })
 })
+
+
+app.post('/api/change-password', async (req, res) => {
+	const { token, newpassword: plainTextPassword } = req.body
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	try {
+		const user = jwt.verify(token, JWT_SECRET)
+
+		const _id = user.id
+
+		const password = await bcrypt.hash(plainTextPassword, 10)
+
+		await User.updateOne(
+			{ _id },
+			{
+				$set: { password }
+			}
+		)
+		res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: ';))' })
+	}
+})
+
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 })
-app.use("/api/user", userRoute)
-io.on("connection", (socket) => {
-    console.log(socket.id);
-    socket.emit("hello", "world!");
-})
-
